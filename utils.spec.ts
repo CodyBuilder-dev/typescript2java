8d9fc0dd597a0352adc03f3d16c087fb394a6d3e
Codybuilder-dev <imspecial1@u.sogang.ac.kr>
2022-04-08 09:54:16 +0900

    test: add test code - kebab2pascal - extractFileName

---
+++ utils.spec.ts
@@ -0,0 +1,26 @@
+import {extractFileName, kebab2pascal} from "./utils";
+
+describe('Utility Test', () =>{
+    describe('extractFileName', () => {
+        test('', () => {
+            expect(extractFileName('/typescript2java/sample/src/sample-file.ts')).toEqual('sample-file.ts')
+            expect(()=> extractFileName('/typescript2java/sample/src/sample-file.js')).toThrowError('not a typescript file')
+        })
+    })
+
+    describe('kebab2pascal', () => {
+        test('',() => {
+            expect(kebab2pascal('file-name-like-this.ts')).toEqual('FileNameLikeThis.java')
+            expect(kebab2pascal('file.name.like.this.ts')).toEqual('FileNameLikeThis.java')
+            expect(kebab2pascal('file--name--like-this.ts')).toEqual('FileNameLikeThis.java')
+            expect(kebab2pascal('file--name.-like-this.ts')).toEqual('FileNameLikeThis.java')
+            expect(kebab2pascal('file--name..like-this..ts')).toEqual('FileNameLikeThis.java')
+            expect(kebab2pascal('file.name.like..this.ts')).toEqual('FileNameLikeThis.java')
+            expect(kebab2pascal('file-Name-like-this.ts')).toEqual('FileNameLikeThis.java')
+            expect(kebab2pascal('file-nAme-like-this.ts')).toEqual('FileNAmeLikeThis.java')
+            expect(kebab2pascal('FileNameLikeThis.ts')).toEqual('FileNameLikeThis.java')
+            expect(kebab2pascal('File.Name.lIkeThis.ts')).toEqual('FileNameLIkeThis.java')
+            expect(kebab2pascal('file-Name-Like-This.ts')).toEqual('FileNameLikeThis.java')
+        })
+    })
+})

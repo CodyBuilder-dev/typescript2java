fd9143b8e6a8023a50d3880cd8749c3945c67245
Codybuilder-dev <imspecial1@u.sogang.ac.kr>
2022-04-08 11:36:21 +0900

    test: add result sample of conversion

---
+++ sample/source/SampleFile.java
@@ -0,0 +1,16 @@
+public class SampleFile {
+private  String variable;
+private  int no;
+SampleFile(String variable,
+int no) 
+{
+        this.variable = variable;
+        this.no = no;
+    }
+
+String method(String userId) 
+{
+        return 'Hello World! from' + userId;    
+    }
+
+}
